698b2745fbfd47d4ea7c6e766b2b1f1d48d7d5a3
tutiagussetiani <tutiagus455@gmail.com>
2022-11-18 12:45:23 +0700

    pertama commit

---
+++ helpers/Menu.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-home "></i>'
+		),
+		
+		array(
+			'path' => 'buku', 
+			'label' => 'Data Buku', 
+			'icon' => '<i class="fa fa-book "></i>'
+		),
+		
+		array(
+			'path' => 'kategori', 
+			'label' => 'Daftar Kategori', 
+			'icon' => '<i class="fa fa-search "></i>'
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Data Migrations', 
+			'icon' => '<i class="fa fa-user-plus "></i>'
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => '<i class="fa fa-key "></i>'
+		),
+		
+		array(
+			'path' => 'role', 
+			'label' => 'Role', 
+			'icon' => '<i class="fa fa-cube "></i>'
+		),
+		
+		array(
+			'path' => 'transaksi', 
+			'label' => 'Data Transaksi', 
+			'icon' => '<i class="fa fa-dollar "></i>'
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Data Users', 
+			'icon' => '<i class="fa fa-users "></i>'
+		)
+	);
+		
+	
+	
+}
